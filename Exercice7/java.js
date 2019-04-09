d8874b214696d23f4f0bd320cc7e2caee01b1ebc
Yomab <mabire.yoan@gmail.com>
2019-04-09 13:24:24 +0200

    Exercice 3 terminé

---
+++ Exercice7/java.js
@@ -0,0 +1,6 @@
+function Valider() {
+var number1 = document.getElementById('shoeSize').value;
+var number2 = document.getElementById('yearOfBirth').value;
+
+alert((number1 * 2) +5) * 50 - number2 + 1766);
+}
---
+++ Partie3/Exercice3/java.js
@@ -0,0 +1,9 @@
+function changecolorGreen(){
+document.getElementById("text").style.color = 'green';
+}
+function changecolorRed(){
+document.getElementById("text").style.color = 'red';
+}
+function changecolorBlue(){
+document.getElementById("text").style.color = 'blue';
+}
